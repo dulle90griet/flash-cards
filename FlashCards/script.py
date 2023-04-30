4abf846bdef4df446ff5e39fb5f796c9e457ebe9
Donald Futers <donald.futers@gmail.com>
2023-04-30 01:53:20 +0100

    Implemented writing new deck to CSV file. Also did light cleanup of previous code and commented it more helpfully.

--- FlashCards/script.py
+++ FlashCards/script.py
@@ -5,7 +5,7 @@ from os import listdir
 from os.path import isfile, join, dirname, abspath
 
 # Generates an ASCII title box and returns it as a string
-def title_box(title_string):
+def title_box(title_string, new_line = 0):
     # First, generate the title string and space it
     # title_string = f"CARD NO. {card_no + 1}"
     for i in range(1, len(title_string)):
@@ -21,13 +21,18 @@ def title_box(title_string):
     mid_line = "\n||  " + title_string + "  ||\n"
     card_string = top_bottom + mid_line + top_bottom
 
+    # If required, add an additional carriage return at the end
+    if new_line == 1:
+        card_string += "\n"
+
     # Return the result
     return card_string
 
-def new_line():
+# Prints a blank line
+def blank_line():
     print("")
 
-testDict = {"A": 1}
+# Define variables
 mypath = dirname(abspath(__file__))
 sleep_time = 1
 max_file_name_len = 20
@@ -61,7 +66,7 @@ if (len(file_list) < 1):
     taking_content = True
 
     while(taking_content):
-        print(title_box(f"CARD NO. {i + 1}") + "\n")
+        print(title_box(f"CARD NO. {i + 1}", 1))
         if i == 0:
             print("What would you like your first card to be?\n")
         valueA = input(f"Enter side A for card {i+1}, " \
@@ -70,35 +75,38 @@ if (len(file_list) < 1):
                 "then press [Enter].\n: ")
         new_deck.append([valueA, valueB])
         print(f"\nGot it. Value A is '{valueA}'. Value B is '{valueB}'.")
-        more_to_add = input("Would you like to add another card " \
-                "to this deck? [Y/N]\n: ").upper()
-        if(more_to_add == "Y" or more_to_add == ""):
-            i += 1
-        else:
+        more_to_add = input("Press [Enter] if you would like to add " \
+                "another card, or type N if you're finished. " \
+                "\n: ").upper()
+        if(more_to_add in ["N", "NO"]):
             taking_content = False
-        new_line()
+        else:
+            i += 1
+        blank_line()
 
-    deck_title = input("Finally, what title would you like to give this deck? (Up to 30 characters.)\n: ")[:30]
+    deck_title = input("Finally, what title would you like to give " \
+            "this deck? (Up to 30 characters.)\n: ")[:30]
 
     # Print out the completed deck, with title box
     print("\nGreat. Here's your deck in review:\n")
     time.sleep(sleep_time / 3)
-    print(title_box(deck_title.upper()) + "\n")
+    print(title_box(deck_title.upper(), 1))
     time.sleep(sleep_time / 3)
     
     # Loop through the cards in the deck and print them
     deck_size = len(new_deck)
     num_width = len(str(deck_size + 1))
+    tab_size = num_width + 5
     for i in range(deck_size):
         # Print the first line
         line_str = f"{i + 1}."
-        for j in range(num_width + 5 - len(line_str)):
+        for j in range(tab_size - len(line_str)):
             line_str += " "
         line_str += f"Side A.    {new_deck[i][0]}"
         print(line_str)
         # Print the second line
         line_str = ""
-        for j in range(num_width + 5):
+        for j in range(tab_size):
             line_str += " "
         line_str += f"Side B.    {new_deck[i][1]}"
         print(line_str + "\n")
@@ -116,7 +124,7 @@ if (len(file_list) < 1):
         time.sleep(sleep_time)
         restart = input("Would you like to start again? [Y/N]\n" \
                 "If not, PyFlashCards will quit.\n: ").upper()
-        if(restart == "Y" or restart == ""):
+        if(restart in ["Y", "YES", ""]):
             # TK -- call deck creation function
             pass
         else:
@@ -129,17 +137,37 @@ if (len(file_list) < 1):
 
         # If the filename is already taken, add a number to the end
         i = 0
-        while os.path.isfile(file_name_clean + ".csv"):
+        while isfile(file_name_clean + ".csv"):
             i += 1
             affix_str = f"({i})"
+            # Calculate available length left after addition of affix
             new_max_len = max_file_name_len - len(affix_str)
             if i > 1:
+                # An affix already exists. Take the length of
+                # everything from the last opening parenthesis in the
+                # file name onwards, inclusive
                 affix_len = len(file_name_clean.split("(")[1]) + 1
+                # Use it to strip out the old affix
                 end_indx = len(file_name_clean) - affix_len
                 file_name_clean = file_name_clean[:end_indx]
+            # Make sure there's space for the new affix, and add it
             if len(file_name_clean) > new_max_len:
                 file_name_clean = file_name_clean[:new_max_len]
             file_name_clean += affix_str
+
+        # Finally, add the filetype extension
+        file_name_clean += ".csv"
+    
+    # Open the file for writing
+    with open(file_name_clean, "x", newline="") as csv_file:
+        field_names = ["side A", "side B"]
+        csv_writer = csv.DictWriter(csv_file, delimiter=",", \
+                quotechar="^", quoting=csv.QUOTE_MINIMAL, \
+                fieldnames = field_names)
+        csv_writer.writeheader()
+        # Loop through the new deck and write each line
+        for card in new_deck:
+            csv_writer.writerow({"side A": card[0], "side B": card[1]})
 
 
 # Otherwise, give them the loading list
